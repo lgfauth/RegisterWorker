42523284c6348aebcb6e84a9a43d4d86e0543626
Luan Fauth <luan.fauth@outlook.com>
2025-03-10 22:28:54 -0300

    remoção do consumo de mensagens para analisar problemas no envio da api

--- src/Worker/Worker.cs
+++ src/Worker/Worker.cs
@@ -15,8 +15,12 @@ namespace RegisterWorker
 
         protected override async Task ExecuteAsync(CancellationToken stoppingToken)
         {
-            await _connectionManager.InitializeAsync();
-            await _consumer.StartConsumingAsync(stoppingToken);
+            //await _connectionManager.InitializeAsync();
+            //await _consumer.StartConsumingAsync(stoppingToken);
+
+            Console.WriteLine("Worker está funcionando...");
+
+            await Task.Delay(TimeSpan.FromSeconds(15).Milliseconds);
         }
 
         public override void Dispose()
